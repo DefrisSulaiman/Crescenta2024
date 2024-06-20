5451069b60a7b9e935e0332cf07e49c081f77013
DefrisSulaiman <daffa0504.ra@gmail.com>
2024-06-20 11:20:48 +0700

    second commit

--- script/script.js
+++ script/script.js
@@ -1,37 +1,90 @@
-window.onload = function() {
-    // Ketika semua elemen telah dimuat, sembunyikan loader
-    let loader = document.getElementById("loader");
-    loader.style.display = "none";
+window.onload = function () {
+  // Ketika semua elemen telah dimuat, sembunyikan loader
+  let loader = document.getElementById("loader");
+  loader.style.display = "none";
 };
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
+
 document.addEventListener('DOMContentLoaded', () => {
-    const faqs = document.querySelectorAll('.faq');
-  
-    faqs.forEach(faq => {
-      const question = faq.querySelector('.question');
-      const answer = faq.querySelector('.answer');
-      const caret = question.querySelector('img');
-  
-      question.addEventListener('click', () => {
-        // Tutup semua FAQ lain
-        faqs.forEach(otherFaq => {
-          if (otherFaq !== faq) {
-            const otherAnswer = otherFaq.querySelector('.answer');
-            const otherCaret = otherFaq.querySelector('.question img');
-            otherAnswer.classList.remove('answer-visible');
-            otherCaret.classList.remove('caret-rotate');
-          }
-        });
-  
-        // Toggle FAQ yang diklik
-        const isVisible = answer.classList.toggle('answer-visible');
-        requestAnimationFrame(() => {
-          if (isVisible) {
-            caret.classList.add('caret-rotate');
-          } else {
-            caret.classList.remove('caret-rotate');
-          }
-        });
+  const faqs = document.querySelectorAll('.faq');
+
+  faqs.forEach(faq => {
+    const question = faq.querySelector('.question');
+    const answer = faq.querySelector('.answer');
+    const caret = question.querySelector('img');
+
+    question.addEventListener('click', () => {
+      // Tutup semua FAQ lain
+      faqs.forEach(otherFaq => {
+        if (otherFaq !== faq) {
+          const otherAnswer = otherFaq.querySelector('.answer');
+          const otherCaret = otherFaq.querySelector('.question img');
+          otherAnswer.classList.remove('answer-visible');
+          otherCaret.classList.remove('caret-rotate');
+        }
+      });
+
+      // Toggle FAQ yang diklik
+      const isVisible = answer.classList.toggle('answer-visible');
+      requestAnimationFrame(() => {
+        if (isVisible) {
+          caret.classList.add('caret-rotate');
+        } else {
+          caret.classList.remove('caret-rotate');
+        }
       });
     });
   });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  window.addEventListener('scroll', function () {
+    console.log("Scroll Y: ", window.scrollY, "50vh: ", window.innerHeight * 0.5);
+    const navbar = document.querySelector('nav');
+    const mobile = document.querySelector('.mob')
+
+    if (window.scrollY >= window.innerHeight * 0.2) {
+      navbar.style.width = '70%';
+      navbar.style.borderRadius = '50px';
+      navbar.style.backgroundColor = 'rgba(255, 255, 255, 0.7)';
+      navbar.style.backdropFilter = 'blur(25px)';
+      navbar.style.marginTop = '20px';
+
+      mobile.style.width = '70%'
+      mobile.style.paddingTop = '0px'
+      mobile.style.marginTop = '120px'
+      mobile.style.backgroundColor = 'rgba(255, 255, 255, 0.7)';
+      mobile.style.backdropFilter = 'blur(25px)';
+      mobile.style.borderRadius = '20px';
+    } else {
+      navbar.style.width = '100%';
+      navbar.style.borderRadius = '0';
+      navbar.style.marginTop = '0';
+
+      mobile.style.width = '100%'
+      mobile.style.paddingTop = '80px'
+      mobile.style.marginTop = '0px'
+      mobile.style.borderRadius = '0px';
+    }
+  });
+});
+
+document.getElementById('checkbox').addEventListener('change', function () {
+  const nav = document.querySelector('.mob');
+  if (this.checked) {
+    nav.style.display = 'block';
+  } else {
+    nav.style.display = 'none';
+  }
+});
+
