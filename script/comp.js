d61752d5a2fcf4132947e90ab2dde215554dc418
DefrisSulaiman <daffa0504.ra@gmail.com>
2024-07-04 12:43:09 +0700

    final 2

--- script/comp.js
+++ script/comp.js
@@ -1,7 +1,7 @@
 // script.js
 document.addEventListener('DOMContentLoaded', (event) => {
     const buttons = document.querySelectorAll('.redirect');
-    const newPath = 'https://drive.google.com/file/d/1Zljf3ynlZ7EybNSNZv9JnHAAzSz2yjgC/view'; // Ganti dengan path yang diinginkan
+    const newPath = 'https://drive.google.com/file/d/1gC7fJ9Tr-zkg753G5HZJm_kacg_OlxQn/view'; // Ganti dengan path yang diinginkan
 
     buttons.forEach(button => {
         button.addEventListener('click', () => {
